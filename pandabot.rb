2473f93cb0e135f660f884a5081cfc13562b50da
Brennan Willingham <brennan.willingham@gmail.com>
2020-06-21 10:44:35 -0500

    Add happy birthday miceli

--- pandabot.rb
+++ pandabot.rb
@@ -12,6 +12,7 @@ end
 
 panda.voice_state_update do |event|
   head_st = 'Robert Batton'
+  miceli = 'Miceli'
   server = event.server
   chat_room = PandaHelper.get_chat_room(server)
 
@@ -19,7 +20,11 @@ panda.voice_state_update do |event|
     chat_room.send_message "Bobahito in the hoooouse!\nhttps://media.giphy.com/media/tlLBddTfaJmJG/giphy.gif"
   end
 
-  chat_room.send_message 'https://tenor.com/TGwe.gif' if PandaHelper.bob_left_public?(event, head_st)
+  chat_room.send_message "That's all folks!\nhttps://tenor.com/TGwe.gif" if PandaHelper.bob_left_public?(event, head_st)
+
+  if PandaHelper.miceli_entered_public?(event, miceli)
+    chat_room.send_message "Happy Birthday @Miceli!\nhttps://tenor.com/view/claptrap-robot-gif-13502248\nYou know.... as our new moderator you should probably contact staff to figure out what that means!"
+  end
 end
 
 panda.run
